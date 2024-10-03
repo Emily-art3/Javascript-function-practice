44f2be9517a4acb0ccc35a474da5e6dbf8a00b23
Emily M <emilymwendwa390@gmail.com>
2024-10-03 12:43:48 +0300

    no return value

---
+++ function-practice-js/scenario2.js
@@ -0,0 +1,8 @@
+//function does not return any value
+function logUserAction(userAction){
+console.log(`User performed: ${userAction}`)
+}
+logUserAction("Login");
+logUserAction("View Profile");
+logUserAction("Logout");
+logUserAction("Add Item to Cart");
