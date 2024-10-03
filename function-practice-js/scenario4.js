4378b97997cebe7fe7456f3db4d75272951711f9
Emily M <emilymwendwa390@gmail.com>
2024-10-03 13:09:18 +0300

    calculus based physics

---
+++ function-practice-js/scenario4.js
@@ -0,0 +1,8 @@
+//function physics problem calculus based
+function calculateVelocity(initialVelocity, acceleration, time){
+    return initialVelocity + (acceleration * time)
+}
+console.log(calculateVelocity(5, 2, 3));
+console.log(calculateVelocity(0, 10, 5));
+console.log(calculateVelocity(20, -3, 4));
+console.log(calculateVelocity(15, 1, 10));
